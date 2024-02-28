3d81d4e785709187449f8c861b952c11a89f7b1c
ibragimovxusan <lazy.devsloper@gmail.com>
2024-02-28 07:18:01 +0500

    Add Company model to admin.py

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Company
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ["id", "username", "email", "role", "is_active", "is_staff", "is_superuser"]
     list_display_links = ["id", "username"]
+
+
+admin.site.register(Company)

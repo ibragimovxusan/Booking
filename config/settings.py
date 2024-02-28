cd94fe1d90580ef84f3dd1ee2696428c6a9dfb19
ibragimovxusan <lazy.devsloper@gmail.com>
2024-02-28 06:50:52 +0500

    Update STATIC_ROOT and STATICFILES_DIRS paths

--- config/settings.py
+++ config/settings.py
@@ -131,10 +131,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'staticfiles'
+    BASE_DIR / 'static'
 ]
 
 MEDIA_URL = '/media/'
